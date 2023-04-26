7c9a45130dccda05d37dc157eea303d6e3d05cb1
Sorenzo <sorenzo88@gmail.com>
2023-04-26 18:42:28 +0200

    test2

--- src/main.ts
+++ src/main.ts
@@ -1,80 +1,166 @@
 import './style.css'
 
 const canvas = document.getElementById("tutorial") as HTMLCanvasElement;
+const cursor = document.getElementById("cursor") as HTMLCanvasElement;
+const ctx = canvas.getContext("2d", { willReadFrequently: true }) as CanvasRenderingContext2D;
+const ctxGrid = canvas.getContext("2d", { willReadFrequently: true }) as CanvasRenderingContext2D;
+const testBtn = document.querySelector('#testBtn') as HTMLCanvasElement;
 
-var displayWidth = 50;
-var displayHeight = 50;
+canvas.width = 800;
+canvas.height = 800;
+
+function drawGrid(ctx: CanvasRenderingContext2D, width: number, height: number, cellWidth: number, cellHeight: number) {
+  ctx.beginPath();
+  ctx.strokeStyle = "#ccc";
+
+  for(let i=0; i <= width; i++) {
+    ctx.moveTo(i * cellWidth, 0);
+    ctx.lineTo(i * cellWidth, height);
+  }
+
+  for(let i=0; i <= height; i++) {
+    ctx.moveTo(0, i * cellHeight);
+    ctx.lineTo(width, i * cellHeight);
+  }
+
+  ctx.stroke();
+}
+
+drawGrid(ctxGrid, canvas.width, canvas.height, 10, 10);
+
+canvas.addEventListener('mouseover', (e) => {
+  cursor.style.left = `${e.clientX}px`;
+  cursor.style.top = `${e.clientY}px`;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+function isBlack(pixelData: Uint8ClampedArray) {
+  if(
+    (pixelData[0] == 0) &&
+    (pixelData[1] == 0) &&
+    (pixelData[2] == 0) &&
+    (pixelData[3] == 255)
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
 const test = () => {
-  console.log('test')
-  const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
   let pixelData = ctx.getImageData(1, 1, 1, 1).data;
-  let caseVivante = [];
-  let caseMorte = [];
+  
+  let nombreDeGeneration = 0;
 
-  for (let x = 0; x < displayWidth; x++) {
-    for (let y = 0; y < displayHeight; y++) {
-      let nbCases = 0;
+  while(nombreDeGeneration < 1) {
+    let caseVivante: [] = [];
+    let caseMorte: [] = [];
+    
+    for (let x = 0; x < displayWidth; x++) {
+      for (let y = 0; y < displayHeight; y++) {
+        let nbCases = 0;
 
-      for(let i = x-1; i < x+2; i++) {
-        for(let j = y-1; j < y+2; j++) {
-          
-          if(i == x && j == y) {
-            continue;
-          } else {
-            pixelData = ctx.getImageData(x, y, 1, 1).data;
-            if(
-              (pixelData[0] == 0) &&
-              (pixelData[1] == 0) &&
-              (pixelData[2] == 0) &&
-              (pixelData[3] == 255)
-            ) {
+        pixelData = ctx.getImageData(x, y, 1, 1).data;
 
-              pixelData = ctx.getImageData(i, j, 1, 1).data;
+        if(isBlack(pixelData)) {
+          console.log(x, y);
+          nbCases++;
+        }
 
+      
+        for(let i = x-1; i < x+2; i++) {
+          for(let j = y-1; j < y+2; j++) {
+            
+            if(i == x && j == y) {
+              continue;
+            } else {
+              
               if(
                 (pixelData[0] == 0) &&
                 (pixelData[1] == 0) &&
                 (pixelData[2] == 0) &&
                 (pixelData[3] == 255)
               ) {
-                nbCases++;
+  
+                pixelData = ctx.getImageData(i, j, 1, 1).data;
+  
+                if(
+                  (pixelData[0] == 0) &&
+                  (pixelData[1] == 0) &&
+                  (pixelData[2] == 0) &&
+                  (pixelData[3] == 255)
+                ) {
+                  nbCases++;
+                }
+  
+                
+                //console.log(pixelData[0], pixelData[1], pixelData[2], pixelData[3])
               }
-
-              
-              //console.log(pixelData[0], pixelData[1], pixelData[2], pixelData[3])
             }
           }
         }
-      }
 
-      if(
-        (pixelData[0] == 0) &&
-        (pixelData[1] == 0) &&
-        (pixelData[2] == 0) &&
-        (pixelData[3] == 255)
-      ) {
-        if (nbCases == 3){
-          caseVivante.push([x, y]);
-        } else{
-          caseMorte.push([x, y]);
-        }
-      } else {
-        if (nbCases == 3 || nbCases == 2){
-          caseVivante.push([x, y]);
+        
+  
+        if(
+          (pixelData[0] == 0) &&
+          (pixelData[1] == 0) &&
+          (pixelData[2] == 0) &&
+          (pixelData[3] == 255)
+        ) {
+          if (nbCases == 3){
+            caseVivante.push([x, y]);
+          } else{
+            caseMorte.push([x, y]);
+          }
         } else {
-          caseMorte.push([x, y]);
+          if (nbCases == 3 || nbCases == 2){
+            caseVivante.push([x, y]);
+          } else {
+            caseMorte.push([x, y]);
+          }
         }
+        
       }
-      
     }
+
+
+    
+    for(let i = 0; i < caseVivante.length; i++) {
+      ctx.fillStyle = "#000000";
+      ctx.fillRect(caseVivante[i][0], caseVivante[i][1], 1, 1);
+    }
+  
+    for(let i = 0; i < caseMorte.length; i++) {
+      ctx.fillStyle = "#FFFFFF";
+      ctx.fillRect(caseMorte[i][0], caseMorte[i][1], 1, 1);
+    }
+
+    nombreDeGeneration++
+
+    if(nombreDeGeneration == 100){ console.log(nombreDeGeneration) }
   }
+
   
   
   
-  
-  
-  /*
       
       for(let i = x-1; i < x+2; i++) {
         for(let j = y-1; j < y+2; j++) {
@@ -91,7 +177,7 @@ const test = () => {
             //console.log(pixelData[0], pixelData[1], pixelData[2], pixelData[3])
           }
           
-          /*
+        
           if(i == x && j == y) {
             continue;
           } else {
@@ -131,49 +217,7 @@ const test = () => {
           caseMorte.push([x, y]);
         }
       }
-      */
-  
-  
-  
-
-
-
-  for(let i = 0; i < caseVivante.length; i++) {
-    ctx.fillStyle = "#000000";
-    ctx.fillRect(caseVivante[i][0], caseVivante[i][1], 1, 1);
-  }
-
-  for(let i = 0; i < caseMorte.length; i++) {
-    ctx.fillStyle = "#FFFFFF";
-    ctx.fillRect(caseMorte[i][0], caseMorte[i][1], 1, 1);
-  }
-  
 }
 
-const testBtn = document.querySelector('#testBtn') as HTMLCanvasElement 
 
-testBtn.addEventListener('click', test)
-
-window.onload = function() {
-  var scale = 3;
-  canvas.style.width = displayWidth + 'px';
-  canvas.style.height = displayHeight + 'px';
- 
-  
-  console.log('window.onload')
-  if (canvas.getContext) {
-    const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
-
-    ctx.fillRect(25, 25, 1, 1);
-    ctx.fillRect(25, 26, 1, 1);
-    ctx.fillRect(25, 27, 1, 1);
-
-    /*
-    for(let i = 0; i < 2; i++) {
-      ctx.fillRect(25, i, 1, 1);
-    }
-    */
-  } else {
-    // canvas-unsupported code here
-  }
-}
+*/
